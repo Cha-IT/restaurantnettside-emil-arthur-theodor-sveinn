3e0dd11e3d068a72ed37e41391b5535af5724412
ArtonLong <artonlong@gmail.com>
2024-03-14 14:40:26 +0100

    added the rest of the drop down

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var starterBtn = document.querySelector("#starterBtn");
 var sideBtn = document.querySelector("#sideBtn");
+var breakfastBtn = document.querySelector("#breakfastBtn");
+var dessertBtn = document.querySelector("#dessertBtn");
+var vegitarianBtn = document.querySelector("#vegitarianBtn");
 
 starterBtn.onclick = () => {
   var starter = document.querySelector("#starter-dropDown");
@@ -9,6 +12,18 @@ starterBtn.onclick = () => {
 sideBtn.onclick = () => {
   var side = document.querySelector("#side-dropDown");
   show_list(side);
+};
+breakfastBtn.onclick = () => {
+  var breakfast = document.querySelector("#breakfast-dropDown");
+  show_list(breakfast);
+};
+dessertBtn.onclick = () => {
+  var dessert = document.querySelector("#dessert-dropDown");
+  show_list(dessert);
+};
+vegitarianBtn.onclick = () => {
+  var vegitarian = document.querySelector("#vegitarian-dropDown");
+  show_list(vegitarian);
 };
 
 function show_list(dropDown) {
