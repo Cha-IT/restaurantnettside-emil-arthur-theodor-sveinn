97150273192d9e260f298295ee7062501322c3b9
Beanos <79772176+BeanosDev@users.noreply.github.com>
2024-03-14 15:24:34 +0100

    Finished API

--- emiltest/script.js
+++ emiltest/script.js
@@ -8,9 +8,17 @@ const filtercaturl='https://www.themealdb.com/api/json/v1/1/filter.php?c=' + cat
 Http2.open("GET", filtercaturl);
 Http2.send();
 Http2.onreadystatechange = function() {
-    if (Http2.readyState == XMLHttpRequest.DONE) {
-        const obj = JSON.parse(Http2.responseText);
-        console.log(obj);
-        console.log(obj['meals'][Math.floor(Math.random() * Object.keys(obj['meals']).length)]['strMeal']);
-    }
+	if (Http2.readyState == XMLHttpRequest.DONE) {
+		const obj = JSON.parse(Http2.responseText);
+		console.log(obj);
+		if (Object.keys(obj['meals']).length >= 5) {
+			for (let i = 0; i < 5; i++) {
+				console.log(obj['meals'][i]['strMeal']);
+			}
+		} else {
+			for (let i = 0; i < Object.keys(obj['meals']).length; i++) {
+				console.log(obj['meals'][i]['strMeal']);
+			}
+		}
+	}
 }
