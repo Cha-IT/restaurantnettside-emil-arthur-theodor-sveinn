8f79b2f30937744d13431e0d207ea98d01f8dc9c
ArtonLong <artonlong@gmail.com>
2024-03-14 14:18:52 +0100

    made sure drop down works

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 var starterBtn = document.querySelector("#starterBtn");
+var sideBtn = document.querySelector("#sideBtn");
 
 starterBtn.onclick = () => {
-  var starter = document.querySelector("#starter");
+  var starter = document.querySelector("#starter-dropDown");
   show_list(starter);
 };
+
+sideBtn.onclick = () => {
+  var side = document.querySelector("#side-dropDown");
+  show_list(side);
+};
+
 function show_list(dropDown) {
   if (dropDown.style.display == "block") {
     dropDown.style.display = "none";
